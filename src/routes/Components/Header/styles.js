22057027d6fdc8124ddc16a4d20a61795d9b0687
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2021-01-15 09:24:11 -0300

    feat(setting-up): project initial format

---
+++ src/routes/Components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  background: #333355;
+  padding: 10px;
+`
+
+export const Title = styled.h1`
+  color: #eee;
+`
---
+++ src/routes/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from './styles'
+import Header from '../../Components/Header'
+
+const home = () => {
+  return (
+    <Container>
+      <Header title="Dev Mobile" />
+
+      <h3>Categorias</h3>
+    </Container>
+  )
+}
+
+export default home
