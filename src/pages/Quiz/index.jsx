53ee28ac303820c1b4f6b3ecf7b5860380ee0302
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2021-01-17 20:13:10 -0300

    feat(Quiz): Quiz Layout finalized

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,17 +1,63 @@
-import { Container } from './styles'
+import { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { Flex } from 'reflexbox'
+
+import { useQuiz } from '../../hooks/Quiz'
 
 import Header from '../../components/Header'
 import Title from '../../components/Title'
+import DifficultyChip from '../../components/DifficultyChip'
+import { Container, AnswerButton } from './styles'
 
-function Quiz() {
+function Quiz({ location }) {
+  const { push } = useHistory()
+  const { saveAnswer } = useQuiz()
+  const [question, setQuestion] = useState()
+  const [answers, setAnswers] = useState()
+
+  useEffect(() => {
+    if (!location.state) push('/')
+
+    const { question: questionData } = location.state
+    setQuestion(questionData)
+
+    const possibleAnswers = [
+      ...questionData.incorrect_answers,
+      questionData.correct_answer,
+    ]
+
+    const shuffledAnswers = possibleAnswers
+      .map(answer => ({ sort: Math.random(), value: answer }))
+      .sort((first, second) => first.sort - second.sort)
+      .map(answer => answer.value)
+
+    setAnswers(shuffledAnswers)
+  }, [location, push, saveAnswer])
+
   return (
     <>
       <Header title="História" />
       <Container>
-        <Title text="Questão 1" />
+        <Flex marginBottom={4} width={1}>
+          <Title text="Questão 1" />
+          <DifficultyChip difficulty={question?.difficulty} />
+        </Flex>
+
+        <p>{question?.question}</p>
+
+        <Flex marginTop={4} flexDirection="column">
+          {answers?.map(answer => (
+            <AnswerButton key={answer}>{answer}</AnswerButton>
+          ))}
+        </Flex>
       </Container>
     </>
   )
+}
+
+Quiz.propTypes = {
+  location: PropTypes.object,
 }
 
 export default Quiz
--- src/pages/Quiz/styles.js
+++ src/pages/Quiz/styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 100vh;
   margin: 5px;
   padding: 20px;
   background: #ffffff;
@@ -9,4 +8,15 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+`
+export const AnswerButton = styled.button`
+  background: #fff;
+  border: 0;
+  border-radius: 6px;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.35);
+  padding: 20px;
+
+  & + button {
+    margin-top: 15px;
+  }
 `
