9983e2aa437ee014678a110d850396d118dcf994
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2021-01-17 21:43:28 -0300

    feat(Header): Category rule implemented

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { Container, Title } from './styles'
 
 function Header({ title }) {
+  const [text, setText] = useState()
+
+  useEffect(() => {
+    switch (title) {
+      case 'History':
+        setText('História')
+        break
+
+      case 'Geography':
+        setText('Geografia')
+        break
+      case 'Mythology':
+        setText('Mitologia')
+        break
+
+      case 'Sports':
+        setText('Esportes')
+        break
+
+      case 'Politics':
+        setText('Política')
+        break
+
+      case 'General Knowledge':
+        setText('Conhecimentos Gerais')
+        break
+
+      default:
+        setText(title)
+        break
+    }
+  }, [title])
+
   return (
     <Container>
-      <Title>{title}</Title>
+      <Title>{text}</Title>
     </Container>
   )
 }
